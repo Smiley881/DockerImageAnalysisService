6ac794d930f614e2af46d87eef72582a5b601c3e
Pablo_3_16 <ergle.pasha@gmail.com>
2025-04-02 14:01:58 +0300

    Implement tests for parsing input json to struct #2

---
+++ services/base_test.go
@@ -0,0 +1,58 @@
+package services
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParseJsonInputToStruct_WithTag(t *testing.T) {
+
+	// Array
+	input := strings.NewReader(`{"repository":"dockerhub.timeweb.cloud","name":"python","tag":"slim"}`)
+	expectedResult := Input{
+		Repository: "dockerhub.timeweb.cloud",
+		Name:       "python",
+		Tag:        "slim",
+	}
+
+	// Act
+	result, err := parseJsonInputToStruct(input)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Произошла ошибка: %v\n", err)
+	}
+
+	if result.Name != expectedResult.Name || result.Repository != expectedResult.Repository || result.Tag != expectedResult.Tag {
+		t.Errorf(
+			"Получен неверный результат:\nActual:\n- Repository: %s\n- Name: %s\n- Tag: %s\nExpected:\n- Repository: %s\n- Name: %s\n- Tag: %s",
+			result.Repository, result.Name, result.Tag, expectedResult.Repository, expectedResult.Name, expectedResult.Tag,
+		)
+	}
+}
+
+func TestParseJsonInputToStruct_WithoutTag(t *testing.T) {
+
+	// Array
+	input := strings.NewReader(`{"repository":"dockerhub.timeweb.cloud","name":"python"}`)
+	expectedResult := Input{
+		Repository: "dockerhub.timeweb.cloud",
+		Name:       "python",
+		Tag:        "latest",
+	}
+
+	// Act
+	result, err := parseJsonInputToStruct(input)
+
+	// Assert
+	if err != nil {
+		t.Errorf("Произошла ошибка: %v\n", err)
+	}
+
+	if result.Name != expectedResult.Name || result.Repository != expectedResult.Repository || result.Tag != expectedResult.Tag {
+		t.Errorf(
+			"Получен неверный результат:\nActual:\n- Repository: %s\n- Name: %s\n- Tag: %s\nExpected:\n- Repository: %s\n- Name: %s\n- Tag: %s",
+			result.Repository, result.Name, result.Tag, expectedResult.Repository, expectedResult.Name, expectedResult.Tag,
+		)
+	}
+}
